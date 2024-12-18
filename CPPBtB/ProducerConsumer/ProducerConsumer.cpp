f83c68cf8702fe9e76e486616a0d6b4c319fe7a3
Bob-Hoogenboom <bhoogenboom23@gmail.com>
2024-12-18 15:59:35 +0100

    assignment 6
    made both outputs zero in this threading race

--- CPPBtB/ProducerConsumer/ProducerConsumer.cpp
+++ CPPBtB/ProducerConsumer/ProducerConsumer.cpp
@@ -1,20 +1,24 @@
 #include <queue>
 #include <thread>
 #include <iostream>
+#include <mutex>
 
 int counter = 0;
 bool done = false;
 
 std::queue<int> goods;
+std::mutex mtx;  //mutex lock
 
 void producer() {
     std::cout << "Starting producer..." << std::endl;
 
     for (int i = 0; i < 500; ++i) {
+        std::lock_guard<std::mutex> lock(mtx);
         goods.push(i);
         counter++;
     }
 
+    std::lock_guard<std::mutex> lock(mtx);
     done = true;
 
     std::cout << "Finished producer..." << std::endl;
@@ -23,12 +27,22 @@ void producer() {
 void consumer() {
     std::cout << "Starting consumer..." << std::endl;
 
-    while (!done) {
-        while (!goods.empty()) {
-            goods.pop();
-            counter--;
+    while (true) {
+        {
+            std::lock_guard<std::mutex> lock(mtx);
+
+            if (done && goods.empty()) {
+                break;
+            }
+
+            while (!goods.empty()) {
+                goods.pop();
+                counter--;
+            }
         }
     }
+
+    std::lock_guard<std::mutex> lock(mtx);
 
     std::cout << "Finished consumer..." << std::endl;
 }
